93f8a1b81578360221218f6d56d02e8d43456681
Blerim Cazimi <blerim@burd.dk>
2020-07-23 11:11:41 +0200

    Added enums.

---
+++ src/Enums/DeviceType.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace Sparav\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class DeviceType extends Enum
+{
+    const Android = 0;
+}
---
+++ src/Enums/LicenseType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Sparav\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class LicenseType extends Enum
+{
+    const AviraVpn = 0;
+}
