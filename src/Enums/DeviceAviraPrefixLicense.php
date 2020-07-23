f6ef7ed17c2d215baeed23ef76a63f49558b6cf9
Blerim Cazimi <blerim@burd.dk>
2020-07-23 15:59:14 +0200

    Update DeviceAviraPrefixLicense.php

--- src/Enums/DeviceAviraPrefixLicense.php
+++ src/Enums/DeviceAviraPrefixLicense.php
@@ -5,7 +5,7 @@ namespace Sparav\CustomerLicense\Enums;
 
 use BenSampo\Enum\Enum;
 
-class DeviceAviraPrefixLicense
+class DeviceAviraPrefixLicense extends Enum
 {
     const AndroidVpn = "0511";
     const IOSVpn = "0531";
