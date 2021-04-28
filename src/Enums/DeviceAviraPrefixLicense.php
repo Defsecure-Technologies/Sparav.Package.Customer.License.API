c51d61dae49d8c459ca59433f09a85ed56f0fe44
blerimcazimi <blerim@tandbud.dk>
2021-04-28 12:42:47 +0200

    Added prefixes for win + mac.

--- src/Enums/DeviceAviraPrefixLicense.php
+++ src/Enums/DeviceAviraPrefixLicense.php
@@ -9,4 +9,8 @@ class DeviceAviraPrefixLicense extends Enum
 {
     const AndroidVpn = "0511";
     const IOSVpn = "0531";
+    const MacAppstoreVpn = "0541";
+    const MacVpn = "0542";
+    const WindowsAppstoreVpn = "0552";
+    const WindowsVpn = "0551";
 }
